f9b317ee8672fe69ce51c580a12adfbe927f2f2f
John Cyphert <cyphertjohn@gmail.com>
2021-07-16 10:19:48 -0500

    updated to running example

--- src/poly.ml
+++ src/poly.ml
@@ -1,10 +1,8 @@
 open Mylib.Expr
 
-let t = from_string "a2-floor(a*b/e)"
-let t1 = from_string "e1 - e - a"
-let t2 = from_string "b1 - b - a2"
-let t3 = from_string "floor(v*b/e) - floor(v*b1/e1)"
+let t = from_string "1+floor(x-y)"
+let t1 = from_string "x- v b /e"
+let t2 = from_string "b + a b / e - 1 - z"
+let t3 = from_string "y - v z/(e+a)"
 
-let vars_to_keep = ["v"; "a"; "e"; "b"]
-
-(*let (polys, term_map) = Mylib.EqRewriter.rewrite [t;t1;t2;t3] vars_to_keep*)
+let vars_to_keep = ["v"; "a"; "e"]
