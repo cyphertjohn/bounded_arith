8d41ec37dedd0033e97aafed0f4b8ebf6fc485a7
yotamfe <yotam.feldman@gmail.com>
2021-08-09 14:27:07 +0300

    exressions for lower and upper bound of Alex's example

--- bin/poly.ml
+++ bin/poly.ml
@@ -6,3 +6,6 @@ let t2 = from_string "b + a b / e - 1 - z"
 let t3 = from_string "y - v z/(e+a)"
 
 let vars_to_keep = ["v"; "a"; "e"]
+
+let tupper = from_string "floor(v b / e) - floor(v(b+floor(a b / e))/(a + e))"
+let tlower = from_string "floor(v(b+floor(a b / e))/(a + e)) - floor(v b / e)"
