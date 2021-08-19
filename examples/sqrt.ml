e9fa5caee6e3fc0d079577af5a0181c91435eb5d
yotamfe <yotam.feldman@gmail.com>
2021-08-19 18:13:48 +0300

    some cleanup

--- examples/sqrt.ml
+++ examples/sqrt.ml
@@ -1,17 +1,11 @@
 open Bound.Expr
 open Str
 
-(* let t0 = from_string "x - r0" *)
-let t1 = from_string "floor((r0 + floor(x / r0)) / (2)) - r1"
-let t2 = from_string "r1 / x - r2"
-let tf = from_string "r2 - r"
-
 let vname i = String.concat "" ["r"; (string_of_int i); "m"]
-(* let vname i = String.concat "" ["r"; (string_of_int i)] *)
 
 let iter_str r = Str.global_replace (regexp "r0") r "floor(((r0) + floor(x / (r0))) / (2))"
 
-let n = 3
+let n = 4
 
 (* ************************************************************************************ *)
 
@@ -31,24 +25,7 @@ let tupper = Bound.EqRewriter.rewrite background_theory ["x"; (vname 0)] (from_s
 
 (* ************************************************************************************ *)
 
-(* let unrolled_str = ref (vname n)
-for i = 0 to n-1 do
-	let cur = n-i in
-    unrolled_str := global_replace (regexp (vname cur)) (iter_str (vname (cur - 1))) !unrolled_str
-done *)
-
-(* let tupper = from_string (Str.global_replace (regexp "res") !unrolled_str "x - (res)^2") *)
-
-
-(* ************************************************************************************ *)
-
 let tupper_res = Bound.EqRewriter.rewrite [] ["x"; (vname 0)] (Bound.IneqRewriter.rewrite tupper)
-(* 
-	n=2: "-81/16 + 9/8r - 1/16r^(2) - 5/8x + 9/8r^(-1)x + x^(2) - 1/16r^(-2)x^(2) - 1/4x^(2)(1/2r + 1/2r^(-1)x)^(-2) + 9/4x(1/2r + 1/2r^(-1)x)^(-1)" 
-	n=3: "-441/64 + 21/32r - 1/64r^(2) - 21/32x + 21/32r^(-1)x + x^(2) - 1/64r^(-2)x^(2) - 1/16x^(2)(1/2r + 1/2r^(-1)x)^(-2) + 33/16x(1/2r + 1/2r^(-1)x)^(-1) - 1/4x^(2)(1/4r + 1/4r^(-1)x + 1/2x(-3/2 + 1/2r + 1/2r^(-1)x)^(-1))^(-2) + 9/4x(1/4r + 1/4r^(-1)x + 1/2x(-3/2 + 1/2r + 1/2r^(-1)x)^(-1))^(-1)"
-*)
-
-
 (* let tlower_res = Bound.EqRewriter.rewrite [] ["x"; (vname 0)] (Bound.IneqRewriter.rewrite tlower) *)
 
 let p = to_string tupper_res
