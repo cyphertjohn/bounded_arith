e9a831bf16398e741e3032ce8a5fae81608162a0
yotamfe <yotam.feldman@gmail.com>
2021-08-19 18:09:50 +0300

    sqrt: fix bug in target term, add lower bound term

--- examples/sqrt.ml
+++ examples/sqrt.ml
@@ -26,7 +26,8 @@ let equate s1 s2 = from_string (s1 ^ " - " ^ s2)
 let background_theory = [equate (vname n) "res"] @ (List.map (fun cur -> 
 																  equate (vname cur) (iter_str (vname (cur - 1)))) 
 													   (1--n))
-let tupper = Bound.EqRewriter.rewrite background_theory ["x"; (vname 0)] (from_string "x^2 - (res)^2")
+let tupper = Bound.EqRewriter.rewrite background_theory ["x"; (vname 0)] (from_string "x - (res)^2")
+(* let tlower = Bound.EqRewriter.rewrite background_theory ["x"; (vname 0)] (from_string "(res)^2 - x") *)
 
 (* ************************************************************************************ *)
 
@@ -36,15 +37,18 @@ for i = 0 to n-1 do
     unrolled_str := global_replace (regexp (vname cur)) (iter_str (vname (cur - 1))) !unrolled_str
 done *)
 
-(* let tupper = from_string (Str.global_replace (regexp "res") !unrolled_str "x^2 - (res)^2") *)
+(* let tupper = from_string (Str.global_replace (regexp "res") !unrolled_str "x - (res)^2") *)
 
 
 (* ************************************************************************************ *)
 
 let tupper_res = Bound.EqRewriter.rewrite [] ["x"; (vname 0)] (Bound.IneqRewriter.rewrite tupper)
-
-
 (* 
 	n=2: "-81/16 + 9/8r - 1/16r^(2) - 5/8x + 9/8r^(-1)x + x^(2) - 1/16r^(-2)x^(2) - 1/4x^(2)(1/2r + 1/2r^(-1)x)^(-2) + 9/4x(1/2r + 1/2r^(-1)x)^(-1)" 
 	n=3: "-441/64 + 21/32r - 1/64r^(2) - 21/32x + 21/32r^(-1)x + x^(2) - 1/64r^(-2)x^(2) - 1/16x^(2)(1/2r + 1/2r^(-1)x)^(-2) + 33/16x(1/2r + 1/2r^(-1)x)^(-1) - 1/4x^(2)(1/4r + 1/4r^(-1)x + 1/2x(-3/2 + 1/2r + 1/2r^(-1)x)^(-1))^(-2) + 9/4x(1/4r + 1/4r^(-1)x + 1/2x(-3/2 + 1/2r + 1/2r^(-1)x)^(-1))^(-1)"
 *)
+
+
+(* let tlower_res = Bound.EqRewriter.rewrite [] ["x"; (vname 0)] (Bound.IneqRewriter.rewrite tlower) *)
+
+let p = to_string tupper_res
