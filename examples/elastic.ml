a805aa255245516303549eb04ddd8bc1a44ddcca
yotamfe <yotam.feldman@gmail.com>
2021-10-21 11:57:46 +0300

    experimenting with the elastic example's lower bound

--- examples/elastic.ml
+++ examples/elastic.ml
@@ -7,7 +7,7 @@ let t3 = from_string "y - v z/(e+a)"
 let vars_to_keep = ["v"; "a"; "e"; "b";]
 
 (* 79.571205s *)
-let tupper = Bound.EqRewriter.rewrite [t1;t2;t3] 
+(* let tupper = Bound.EqRewriter.rewrite [t1;t2;t3] 
 									  [
 									  	from_string "x - floor(x)";
 									  	from_string "floor(y) + 1 - y";
@@ -16,6 +16,28 @@ let tupper = Bound.EqRewriter.rewrite [t1;t2;t3]
 									  	from_string "1/(e + a)";
 									  ] 
 									  vars_to_keep 
-									  (from_string "floor(x) - floor(y)")
+									  (from_string "floor(x) - floor(y)") *)
 
-(* let tlower = Bound.EqRewriter.rewrite [t1;t2;t3] [] vars_to_keep (from_string "floor(y) - floor(x)") *)
+(* result "1". 65.898197s *)
+let tlower = Bound.EqRewriter.rewrite [t1;t2;t3] 
+									  [
+										from_string "floor(x) + 1 - x";
+									  	from_string "y - floor(y)";
+									  	from_string "(a b / e) - floor(a b / e)";
+									  	from_string "v";
+									  	from_string "1/(e + a)";
+									  ] 
+									  vars_to_keep 
+									  (from_string "floor(y) - floor(x)")
+
+(* result "-floor(be^-1v) + floor(b(a + e)^-1v + (a + e)^-1vfloor(abe^-1))". 3.372992s 
+The reason is that the derivation in the writeup performs inequational reasoning under the floor term,
+which is impossible in the cone. *)
+(* let tlower = Bound.EqRewriter.rewrite [t1;t2;t3] 
+									  [
+									  	from_string "(a b / e) - floor(a b / e)";
+									  	from_string "v";
+									  	from_string "1/(e + a)";
+									  ] 
+									  vars_to_keep 
+									  (from_string "floor(y) - floor(x)") *)
