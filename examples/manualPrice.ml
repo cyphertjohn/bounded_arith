3dbfd544ec267ae838a83abd43e88bb5eb5073d4
yotamfe <yotam.feldman@gmail.com>
2021-10-25 16:55:39 +0300

    manual price example: improved bounds by using the axiom x >= 0 ==> floor(x) >= 0

--- examples/manualPrice.ml
+++ examples/manualPrice.ml
@@ -11,11 +11,15 @@ let tupper = Bound.EqRewriter.rewrite [t1;t2;t3]
 									  	from_string "startPrice - minimumPrice";
 									  	from_string "endTime - startTime";
 									  	from_string "t1 - startTime";
-									  	from_string "floor((startPrice - minimumPrice) / (endTime - startTime)) + 1 - ((startPrice - minimumPrice) / (endTime - startTime))"; (* floor lower bound rule *)
 									  	from_string "(startPrice - minimumPrice) / (endTime - startTime)"; (* division rule *)
+									  	from_string "floor((startPrice - minimumPrice) / (endTime - startTime))"; (* floor positivity rule (obtained from previous) *)
 									  ] 
 									  vars_to_keep 
 									  (from_string "price1")
+(* 
+utop # to_string tupper;;
+- : string = "startPrice"
+*)
 
 let tlower = Bound.EqRewriter.rewrite [t1;t2;t3] 
 									  [
@@ -28,6 +32,10 @@ let tlower = Bound.EqRewriter.rewrite [t1;t2;t3]
 									  ] 
 									  vars_to_keep 
 									  (from_string "-price1")
+(* 
+utop # to_string tlower;;
+- : string = "-minimumPrice"
+*)
 
 let t1_2 = from_string "floor((startPrice - minimumPrice) / (endTime - startTime)) - drop"
 let t2_2 = from_string "(t2 - startTime) drop - diff2"
@@ -42,9 +50,13 @@ let tmonotone = Bound.EqRewriter.rewrite [t1;t2;t3;t1_2;t2_2;t3_2;]
 									  	from_string "t1 - startTime";
 									  	from_string "t2 - t1";
 									  	from_string "endTime - t2";
-									  	from_string "((startPrice - minimumPrice) / (endTime - startTime)) - floor((startPrice - minimumPrice) / (endTime - startTime))"; (* floor upper bound rule *)
-									  	from_string "floor((startPrice - minimumPrice) / (endTime - startTime)) + 1 - ((startPrice - minimumPrice) / (endTime - startTime))"; (* floor lower bound rule *)
 									  	from_string "(startPrice - minimumPrice) / (endTime - startTime)"; (* division rule *)
+									  	from_string "floor((startPrice - minimumPrice) / (endTime - startTime))"; (* floor positivity rule (obtained from previous) *)
+									  	from_string "floor((startPrice - minimumPrice) / (endTime - startTime)) + 1 - ((startPrice - minimumPrice) / (endTime - startTime))"; (* floor lower bound rule *)
 									  ] 
 									  vars_to_keep 
 									  (from_string "price2 - price1")
+(* 
+utop # to_string tmonotone;;
+- : string = "0" 
+*)
