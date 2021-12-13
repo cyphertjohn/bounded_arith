6f2fe218375c7ce04d1a1e07485d0f2e36999eea
yotamfe <yotam.feldman@gmail.com>
2021-12-13 15:08:07 +0200

    nirn example: example involving multiple equalities fails

--- examples/nirn.ml
+++ examples/nirn.ml
@@ -8,54 +8,54 @@ let transferX = List.map from_string [
 					(** transfer x **)
 					(* transfer from - ignored. total balance remains the same. *)
 					(* calculate fee *)
-						"floor(supply0 priceAtLastFee0 / E18)  - valueAtLastCollectionPriceX";
-						"floor((totalBalance0 - valueAtLastCollectionPriceX) performanceFee) - totalFeesX";
-						"floor(totalFeesX supply0 / (totalBalance0 - totalFeesX)) - equivalentSharesX";
+						(* "floor(supply0 priceAtLastFee0 / E18)  - valueAtLastCollectionPriceX"; *)
+						(* "floor((totalBalance0 - valueAtLastCollectionPriceX) performanceFee) - totalFeesX"; *)
+						(* "floor(totalFeesX supply0 / (totalBalance0 - totalFeesX)) - equivalentSharesX"; *)
 						(* mint *)
-							"supply0 + equivalentSharesX - supplyXmid";
+							(* "supply0 + equivalentSharesX - supplyXmid"; *)
 							"totalBalance0 - totalBalanceXmid"; (* "totalBalance0 + equivalentSharesX - totalBalanceXmid"; *)
 						(* rest of calculate fee *)
-						"floor((totalBalance0 E18) / (supplyXmid)) - priceAtLastFeeX";
+						(* "floor((totalBalance0 E18) / (supplyXmid)) - priceAtLastFeeX"; *)
 					(* rest of transfer x *)
-					"floor((x supplyXmid) / (totalBalanceXmid)) - sharesX";
+					(* "floor((x supplyXmid) / (totalBalanceXmid)) - sharesX"; *)
 					(* mint *)
-						"supplyXmid + sharesX - supplyX";
+						(* "supplyXmid + sharesX - supplyX"; *)
 						"totalBalanceXmid - totalBalanceX"; (* "totalBalanceXmid + sharesX - totalBalanceX"; *)
 				] 
 let transferYAfterX = List.map from_string [
 					(** transfer y **)
 					(* transfer from - ignored. total balance remains the same. *)
 					(* calculate fee *)
-						"floor(supplyX priceAtLastFeeX / E18)  - valueAtLastCollectionPriceY";
-						"floor((totalBalanceX - valueAtLastCollectionPriceY) performanceFee) - totalFeesY";
-						"floor(totalFeesY supplyX / (totalBalanceX - totalFeesY)) - equivalentSharesY";
+						(* "floor(supplyX priceAtLastFeeX / E18)  - valueAtLastCollectionPriceY"; *)
+						(* "floor((totalBalanceX - valueAtLastCollectionPriceY) performanceFee) - totalFeesY"; *)
+						(* "floor(totalFeesY supplyX / (totalBalanceX - totalFeesY)) - equivalentSharesY"; *)
 						(* mint *)
-							"supplyX + equivalentSharesY - supplyYmid";
+							(* "supplyX + equivalentSharesY - supplyYmid"; *)
 							"totalBalanceX - totalBalanceYmid"; (* "totalBalanceX + equivalentSharesY - totalBalanceYmid"; *)
 						(* rest of calculate fee *)
-						"floor((totalBalanceX E18) / (supplyYmid)) - priceAtLastFeeY";
+						(* "floor((totalBalanceX E18) / (supplyYmid)) - priceAtLastFeeY"; *)
 					(* rest of transfer y *)
-					"floor((y supplyYmid) / (totalBalanceYmid)) - sharesY";
+					(* "floor((y supplyYmid) / (totalBalanceYmid)) - sharesY"; *)
 					(* mint *)
-						"supplyYmid + sharesY - supplyY";
+						(* "supplyYmid + sharesY - supplyY"; *)
 						"totalBalanceYmid - totalBalanceY"; (* "totalBalanceYmid + sharesY - totalBalanceY"; *)
 				] 
 let transferXplusY = List.map from_string [
 					(** transfer x + y **)
 					(* transfer from - ignored. total balance remains the same. *)
 					(* calculate fee *)
-						"floor(supply0 priceAtLastFee0 / E18)  - valueAtLastCollectionPriceXY";
-						"floor((totalBalance0 - valueAtLastCollectionPriceXY) performanceFee) - totalFeesXY";
-						"floor(totalFeesX supply0 / (totalBalance0 - totalFeesXY)) - equivalentSharesXY";
+						(* "floor(supply0 priceAtLastFee0 / E18)  - valueAtLastCollectionPriceXY"; *)
+						(* "floor((totalBalance0 - valueAtLastCollectionPriceXY) performanceFee) - totalFeesXY"; *)
+						(* "floor(totalFeesX supply0 / (totalBalance0 - totalFeesXY)) - equivalentSharesXY"; *)
 						(* mint *)
-							"supply0 + equivalentSharesXY - supplyXYmid";
+							(* "supply0 + equivalentSharesXY - supplyXYmid"; *)
 							"totalBalance0 - totalBalanceXYmid"; (* "totalBalance0 + equivalentSharesXY - totalBalanceXYmid"; *)
 						(* rest of calculate fee *)
-						"floor((totalBalance0 E18) / (supplyXYmid)) - priceAtLastFeeXY";
+						(* "floor((totalBalance0 E18) / (supplyXYmid)) - priceAtLastFeeXY"; *)
 					(* rest of transfer x + y *)
-					"floor(((x+y) supplyXYmid) / (totalBalanceXYmid)) - sharesXY";
+					(* "floor(((x+y) supplyXYmid) / (totalBalanceXYmid)) - sharesXY"; *)
 					(* mint *)
-						"supplyXYmid + sharesXY - supplyXY";
+						(* "supplyXYmid + sharesXY - supplyXY"; *)
 						"totalBalanceXYmid - totalBalanceXY"; (* "totalBalanceXYmid + sharesXY - totalBalanceXY"; *)
 				] 
 
