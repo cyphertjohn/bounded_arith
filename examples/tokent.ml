b47c1b7453c0a5d1bd9ea569125933140aea2f3b
yotamfe <yotam.feldman@gmail.com>
2022-05-31 22:56:46 +0300

    tokent example: more assumptions - that the amount withdrawn is smaller than the callee's balance and the supply - make the upper bound almost tight. also nice lower bound

--- examples/tokent.ml
+++ examples/tokent.ml
@@ -24,7 +24,7 @@ let withdrawJoined = List.map from_string [
 				]
 
 let tupperAndTlower = Bound.Log.log_time "Rewrite joined no worse" (Bound.Rewriter.rewrite ~sat:3 (withdrawSplit @ withdrawJoined)
-						  (List.map from_string ["x"; "supply0"; "balance0"; "liquidFunds"])
+						  (List.map from_string ["x"; "supply0 - balance0"; "balance0 - 2x"; "liquidFunds"])
 						  vars_to_keep)
    						  [(from_string "balanceSplit - balanceJoined");
 						   from_string "balanceJoined - balanceSplit"]
